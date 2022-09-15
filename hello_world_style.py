d34b709e01da3087a0f886a5f27a878e4bb92502
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-09-15 18:25:04 +0000

    Added Linter, finished program that outputs twolines into the console, added comments.

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Van Nguyen
+# Date: September 14th, 2022
+# Calls a function that prints out two lines
+
+# Function that outputs two lines of text to the console.
+def main():
+    print("Hello World! I am writing in proper style! :D")
+    print("I am testing my main() function.")
+
+if __name__ == "__main__":
+    main()
